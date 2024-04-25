b5a0c612c4fdfa24894cb394e80d1747afd396c7
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-04-25 22:14:25 +0200

    ✨ Create Accommodation page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Accoommodation from './pages/Accommodation'
 
 function App() {
-return <Routes>
-  <Route path="/" element={<Home/>} >
-    
-  </Route>
-</Routes>
+  return (<Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/accommodation" element={<Accoommodation />} />
+  </Routes>
+  )
 }
 
 export default App
