4685754357d2021ba558fb110b34d27b6bda5a32
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-04-25 22:24:46 +0200

    ✨ Install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Accoommodation from './pages/Accommodation'
-import Error from './pages/Error 404'
-import About from './pages/About'
+import Home from './pages/Home/Home'
+import Accoommodation from './pages/Accommodation/Accommodation'
+import Error from './pages/Error/Error 404'
+import About from './pages/About/About'
 
 function App() {
   return (<Routes>
     <Route path="/" element={<Home />} />
     <Route path="/accommodation" element={<Accoommodation />} />
-    <Route path="/error" element={<Error />} />
+    <Route path="*" element={<Error />} />
     <Route path="/about" element={<About />} />
   </Routes>
   )
