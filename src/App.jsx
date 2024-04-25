c2591915dda01c4cd14286a5deb57f2dc502286a
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-04-25 22:17:40 +0200

    ✨ Create About page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Accoommodation from './pages/Accommodation'
 import Error from './pages/Error 404'
+import About from './pages/About'
 
 function App() {
   return (<Routes>
     <Route path="/" element={<Home />} />
     <Route path="/accommodation" element={<Accoommodation />} />
     <Route path="/error" element={<Error />} />
+    <Route path="/about" element={<About />} />
   </Routes>
   )
 }
