6b2be0833bea0b2d0c19a434c98aedfe2576a294
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-04-26 00:12:09 +0200

    💄 Home page > Header & Footer > Add style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,24 @@
+import './Home.css';
+
 export default function Home() {
-    return <div>Hello World !</div>
+    return (<div>
+        <header>
+            <div className="header-container">
+                <img src="../../public/Logo.png" alt="Logo" />
+                <nav>
+                    <ul>
+                        <li>Accueil</li>
+                        <li>A Propos</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <footer>
+            <div className="footer-container">
+                    <img src="../../public/Small_logo.png" alt="Logo" />
+                    <p>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </footer>
+    </div>
+    )
 }
