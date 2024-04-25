821b9d75af571d47814f515edf6f348e592a2d4e
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-04-25 22:16:14 +0200

    ✨ Create Error 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Accoommodation from './pages/Accommodation'
+import Error from './pages/Error 404'
 
 function App() {
   return (<Routes>
     <Route path="/" element={<Home />} />
     <Route path="/accommodation" element={<Accoommodation />} />
+    <Route path="/error" element={<Error />} />
   </Routes>
   )
 }
