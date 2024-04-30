6810b97946ecf59eb0b32c8cd7c2690d327b6e0c
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-04-30 19:54:47 +0200

    ✨ Accommodation page > Component > Carrousel > Create Carrousel

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -15,6 +15,7 @@ export default function Accommodation() {
     const [accommodation, setAccommodation] = useState(null);
     const [isDescriptionOpen, setDescriptionOpen] = useState(false);
     const [isEquipmentsOpen, setEquipmentsOpen] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -31,13 +32,21 @@ export default function Accommodation() {
         fetchData();
     }, [id]);
 
+    const handleNextImage = () => {
+        setCurrentIndex((currentIndex + 1) % accommodation.pictures.length);
+    };
+
+    const handlePrevImage = () => {
+        setCurrentIndex((currentIndex - 1 + accommodation.pictures.length) % accommodation.pictures.length);
+    };
+
     const toggleCollapse = (collapseType, direction) => {
         if (collapseType === 'description') {
             setDescriptionOpen(!isDescriptionOpen);
         } else if (collapseType === 'equipments') {
             setEquipmentsOpen(!isEquipmentsOpen);
         }
-        
+
         const icon = document.querySelector(`.fa-chevron-up.${collapseType}`);
         if (icon) {
             if (direction === 'up') {
@@ -68,7 +77,22 @@ export default function Accommodation() {
             <Header />
             {accommodation && (
                 <main>
-                    <img className="accommodation-img" src={accommodation.cover} alt={accommodation.title} />
+                    <div className="carrousel">
+                        <div className="counter">{currentIndex + 1}/{accommodation.pictures.length}</div>
+                        <img className="accommodation-img" src={accommodation.pictures[currentIndex]} alt={accommodation.title} />
+                        <svg className="chevron-left" width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handlePrevImage}>
+                            {<svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white" />
+                            </svg>
+                            }
+                        </svg>
+                        <svg className="chevron-right" width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handleNextImage}>
+                            {<svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white" />
+                            </svg>
+                            }
+                        </svg>
+                    </div>
                     <div className="accommodation-info">
                         <div>
                             <h2>{accommodation.title}</h2>
