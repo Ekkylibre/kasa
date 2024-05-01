7772d8925442dcbbccfd796ca87f512c601d6a51
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-01 15:17:38 +0200

    🍱 Add collapse.json

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../components/banner/Banner";
+import Collapse from "../../components/collapse/Collapse";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import './about.css';
@@ -7,9 +8,11 @@ export default function About() {
     const text = "";
     const bannerImg = "/img2.jpg";
     
+    
     return <div>
         <Header />
         <Banner text={text} bannerImg={bannerImg}/>
+        <Collapse />
         <Footer />
     </div>
 }
