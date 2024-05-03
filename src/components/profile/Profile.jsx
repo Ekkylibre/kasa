3bc96be6401b7aad8aaf4d052df570476b53d3f9
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-03 22:22:52 +0200

    ✨ Accommodation page > Component > Create Profile

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,7 +3,7 @@ import './profile.css';
 export default function Profile({ host }) {
     return (
         <div className="profile">
-            <p>{host.name}</p>
+            <p className='profile-name'>{host.name}</p>
             <img src={host.picture} alt={host.name} />
         </div>
     )
