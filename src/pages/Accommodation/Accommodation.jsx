a1aa9ee944a180856bb39a7e582dd136cec813fd
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-06 17:27:44 +0200

    ✨ Accommodation page > Component > Create Carousel

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -8,6 +8,7 @@ import Collapse from "../../components/collapse/Collapse";
 import Stars from "../../components/stars/Stars";
 import Profile from "../../components/profile/Profile";
 import AccommodationInfo from "../../components/accommodationInfo/AccommodationInfo";
+import Carousel from "../../components/carousel/Carousel";
 
 export default function Accommodation() {
     const { id } = useParams();
@@ -34,39 +35,20 @@ export default function Accommodation() {
         fetchData();
     }, [id, navigate]);
 
-    const handleNextImage = () => {
-        setCurrentIndex((currentIndex + 1) % accommodation.pictures.length);
-    };
-
-    const handlePrevImage = () => {
-        setCurrentIndex((currentIndex - 1 + accommodation.pictures.length) % accommodation.pictures.length);
-    };
-
     return (
         <>
             <Header />
             {accommodation && (
                 <main>
                     <div className="main-container">
-                        <div className="carrousel">
-                            <div className="counter">{currentIndex + 1}/{accommodation.pictures.length}</div>
-                            <img className="accommodation-img" src={accommodation.pictures[currentIndex]} alt={accommodation.title} />
-                            <svg className="chevron-left" width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handlePrevImage}>
-                                {<svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white" />
-                                </svg>
-                                }
-                            </svg>
-                            <svg className="chevron-right" width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handleNextImage}>
-                                {<svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white" />
-                                </svg>
-                                }
-                            </svg>
-                        </div>
+                        <Carousel
+                            pictures={accommodation.pictures}
+                            currentIndex={currentIndex}
+                            setCurrentIndex={setCurrentIndex}
+                        />
                         <div className="accommodation-info">
                             <div>
-                            <AccommodationInfo title={accommodation.title} location={accommodation.location} />
+                                <AccommodationInfo title={accommodation.title} location={accommodation.location} />
                                 <Tag tags={accommodation.tags} />
                             </div>
                             <div className="host">
